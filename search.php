377a2e75d5a956e5d234af9d2330f0c3be4cf5fa
Jasim Karim <jasimvk@gmail.com>
2022-04-19 23:30:32 +0400

    solution

--- search.php
+++ search.php
@@ -1,28 +1,21 @@
 <?php
     
-    // $file = fopen("file.csv","r");
-    // // Read first line
-    // echo fgets($file);
-    // // Move back to beginning of file
-    // fseek($file,1);
-    // fclose($file);
-    // echo file_get_contents("file.csv");
+    $filename = $argv[1];
+    $column_num = $argv[2];
+    $key = $argv[3];
+    
+    
+    if (($fp = fopen($filename, "r")) !== false) {
+        while (($row = fgetcsv($fp)) !== false) {
+            
+            if($row[$column_num] === $key) {
+                echo  $row[0] . ', ' . $row[1] . ', ' . $row[2] . ', ' . $row[3] . "\n";
+            }
+        }
+        fclose($fp);
+    }
 
-    // if (isset($argc)) {
-          //echo "test".$argv[0];
-          $filename = $argv[1];
-          //echo $filename;
-        //   $file = fopen( $filename, 'r');
-        //     while (($line = fgetcsv($file)) !== FALSE) {
-        //     print_r($line[$argv[2]]);
-        //     }
-        //     fclose($file);
-
-        $csv = array_map('str_getcsv', file($filename));
-        echo $csv[$argv[2]];
-
-    //  }
-  
-?>
+    ?>
+    
 
 
